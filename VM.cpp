e8e03a8f6c5c23e0adf3e4de1be5d6b8d29b1abc
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-11-01 17:13:39 -0200

    alteração de alguns métodos e da função main

--- VM.cpp
+++ VM.cpp
@@ -8,7 +8,8 @@ Fluxo genérico de funcionamento
 
 - A máquina é ligada (liga_desliga)
 - Usuário insere as moedas (setSaldo)
-- Escolhe o produto (escolhe_item)
+- Escolhe o produto (set_item)
+- A máquina calcula se o saldo é suficiente;
 - A máquina libera o produto
 - A máquina calcula o troco (setTroco)
 - Devolve o troco (devolve_troco)
@@ -34,19 +35,20 @@ public:
 	VM(); //construtor
 	
 	void liga_desliga(); //liga ou desliga a máquina, invertendo o estado atual
-	void escolhe_item(int n_item); //escolhe um dos itens disponíveis de acordo com o número digitado pelo usuário
+	void set_item(int n_item); //escolhe um dos itens disponíveis de acordo com o número digitado pelo usuário
 
 
 private:
 	
 	float devolve_troco(); //libera o troco
-	bool libera_produto(int n_item); //libera o produto escolhido
+	string libera_produto(int n_item); //libera o produto escolhido
  	float set_preco(float preco); //
 	int set_quantidade(int quantidade);
 	bool destranca(int s);
 	
 	void setSaldo(float s); //
 	void setTroco(float t);
+	float getSaldo();
 	
 	int quantidadeItem[nTipos];
 	string modelo;
@@ -55,12 +57,12 @@ private:
 	float saldo;
 	float troco;
 	bool trancado;   	
-	bool motor[nSlots];
+	bool motor[nTipos];
     
 };
 
 VM::VM(){
-	for(int i = 0 i < nTipos; i++){
+	for(int i = 0; i < nTipos; i++){
 		quantidadeItem[i] = 0;
 	}
 	
@@ -68,7 +70,7 @@ VM::VM(){
 	
 	ligado = false;
 	
-	for(int i = 0 i < nTipos; i++){
+	for(int i = 0; i < nTipos; i++){
 		precoItem[i] = 0;
 	}
 	
@@ -95,7 +97,7 @@ void VM::set_saldo(float s){
 	
 }
 
-void VM::escolhe_item(int n_item){ //cin >> "Digite o número do item" >>  
+void VM::set_item(int n_item){ //cin >> "Digite o número do item" >>  
 	
 	if(quantidadeItem[n_item] > 0){
 	
@@ -116,10 +118,12 @@ void VM::setTroco(int item){
 }
 
 
-bool VM:: libera_produto(int n_item){
-	motor[n_item] = true;
+string VM:: libera_produto(int n_item){
+	if(this.saldo >= this.precoItem[n_item]){
+		return "Produto liberado ;)";
+	else
+		return "Saldo insuficiente";	
 	
-	return true;
 }
 	
 float VM::devolve_troco(float t){
@@ -128,6 +132,36 @@ float VM::devolve_troco(float t){
 
 int main()
 {
+    VM maq1; //declara um objeto maq1 da classe VM
+    
+    float saldo;
+    int opcao;
+    
+    maq1.liga_desliga(); //liga a máquina
+    
+    cout << "Digite a quantidade de dinheiro inserido: " << endl;
+    cin >> saldo;
+    maq1.setSaldo(saldo);
+    maq1.getSaldo();
+    
+    cout << "Digite o número do produto desejado:" << endl;
+    cout << " (1)   Salgadinho 1 " << endl;
+    cout << " (2)   Salgadinho 2 " << endl;
+    cout << " (3)   Salgadinho 3 " << endl;
+    cout << " (4)         Doce 1 " << endl;
+    cout << " (5)         Doce 2 " << endl;
+    cout << " (6) Refrigerante 1 " << endl;
+    cout << " (7) Refrigerante 2 " << endl;
+    cout << " (8) Refrigerante 3 " << endl;
+    
+    cin >> opcao;
+    maq1.set_item(opcao);
+    
+    
+    
+    m1.libera_produto();
+    
+    
     
     return 0;       
 }
