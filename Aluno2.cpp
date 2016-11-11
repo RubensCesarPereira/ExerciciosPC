48db0b1f80115d1ddb50d9c8775abd8a5e891285
Ranieri Ferreira <rany.ferre@gmail.com>
2016-11-11 20:47:48 -0200

    Update Aluno2.cpp

--- Aluno2.cpp
+++ Aluno2.cpp
@@ -30,7 +30,7 @@ class Aluno{
 		static int cont_ra;
 };
 
-int Aluno::cont_ra;
+int Aluno::cont_ra = 1;
 
 Aluno::Aluno(): RA(cont_ra){
     setNome("Maria");
@@ -52,9 +52,10 @@ Aluno::Aluno(string nome, int nro_notas ): RA(cont_ra){
 	setNroNotas(nro_notas);
 
 	for(int i = 0; i < nro_notas; i++){
-		std::cout << "Digite a nota da prova " << i+1 << std::endl;
+		std::cout << "Digite a nota da prova " << i+1 << ": ";
 		std::cin >> ptr[i];
 	}
+	std::cout << std::endl;
 }
 
 Aluno::~Aluno(){
@@ -66,17 +67,17 @@ void Aluno::setNome(string n){
 }
 
 string Aluno::getNome() const{
-	cout << "getNome rodou" << endl;
+	//cout << "getNome rodou" << endl;
 	return nome;
 }
 
 int Aluno::getRA() const{
-	cout << "getRA rodou" << endl;
+	//cout << "getRA rodou" << endl;
 	return RA;
 }
 
 bool Aluno::setNroNotas(int nr){
-	if (nr < 0) {
+	if (nr > 0) {
         nro_notas = nr;
         return true;
     } else
@@ -84,7 +85,7 @@ bool Aluno::setNroNotas(int nr){
 }
 
 int Aluno::getNroNotas() const{
-	cout << "getNroNotas rodou" << endl;
+	//cout << "getNroNotas rodou" << endl;
 	return nro_notas;
 }
 
@@ -101,7 +102,7 @@ bool Aluno::setNota(int pos, double *valor){
 
 bool Aluno::getNota(int pos, double *valor) const{
     pos = pos-1;
-	cout << "getNota rodou" << endl;
+	//cout << "getNota rodou" << endl;
 	if(pos >= 0 && pos <= getNroNotas()){
 		*valor = ptr[pos];
 		return true;
@@ -115,55 +116,51 @@ bool Aluno::getNota(int pos, double *valor) const{
 
 int main(int argc, char** argv) {
 	Aluno aluno1; //declara um aluno 1 com valor padrao
-	std::cout << "Nome do aluno 1:";
+	std::cout << "Nome do aluno 1: ";
 	std::cout << aluno1.getNome() << std::endl;
 	std::cout << "-------------------------------------" << endl;
 
-	std::cout << "RA do aluno 1:";
+	std::cout << "RA do aluno 1: ";
 	std::cout << aluno1.getRA() << std::endl;
 	std::cout << "-------------------------------------" << endl;
 
-	std::cout << "Numero de notas do aluno 1:";
+	std::cout << "Numero de notas do aluno 1: ";
 	std::cout << aluno1.getNroNotas() << std::endl;
 	std::cout << "-------------------------------------" << endl;
 
-	std::cout << "Nota 1 do aluno 1:";
 	double nota1;
-	std::cout << aluno1.getNota(1, &nota1) <<endl;
-	std::cout << "-------------------------------------" << endl;
+	aluno1.getNota(1, &nota1);
 
-	std::cout << "Nota 1 do aluno 1 na variavel:" << endl;
+	std::cout << "Nota 1 do aluno 1: " ;
 	std::cout << nota1 << std::endl;
 
-	std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" "<< endl;
+	std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ " << endl;
     std::cout << endl;
 
 	std::cout << "Numero de notas do aluno declarado como 5" << endl;
+
 	Aluno aluno2("Joao", 5);
 
-	std::cout << "Nome do aluno 2: " << endl;
+	std::cout << "Nome do aluno 2: ";
 	std::cout << aluno2.getNome() << std::endl;
 	std::cout << "-------------------------------------" << endl;
 
-	std::cout << "RA do aluno 2: " << endl;
+	std::cout << "RA do aluno 2: ";
 	std::cout << aluno2.getRA() << std::endl;
 	std::cout << "-------------------------------------" << endl;
 
-    int teste;
-	std::cout << "Numero de notas do aluno 2: " << endl;
-	teste = aluno2.getNroNotas();
-	std::cout << teste << std::endl;
+	std::cout << "Numero de notas do aluno 2: ";
+	std::cout << aluno2.getNroNotas() << std::endl;
 	std::cout << "-------------------------------------" << endl;
 
-	//----------------- ERRO, a nota sai sempre 1  --------------------
-	std::cout << "Nota 1 do aluno 2: " << endl;
+	std::cout << "Nota 1 do aluno 2: ";
 	double nota2;
 	aluno2.getNota(1, &nota2);
 	std::cout << nota2 <<endl;
 	std::cout << "-------------------------------------" << endl;
     double nota3;
 	aluno2.getNota(4, &nota3);
-	std::cout << "Nota 4 do aluno 2 na variavel:" << endl;
+	std::cout << "Nota 4 do aluno 2: ";
 	std::cout << nota3 << std::endl;
 
 	return 0;
