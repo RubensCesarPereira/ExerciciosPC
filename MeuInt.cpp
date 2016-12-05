84324eb153799144ab3966853d764cf973683937
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-12-05 13:34:16 -0200

    classe MeuInt
    classe "cópia" da int e cálculo das raízes de uma equação na classe main

--- MeuInt.cpp
+++ MeuInt.cpp
@@ -1,1 +1,87 @@
-#include <iostream>using namespace std;class MeuInt{  public:  MeuInt(int i=0);  int operator+(int);  int operator-(int);  int operator*(int);  int operator/(int);  int getInteiro();  void setInteiro(int);   private:  int inteiro;   };MeuInt::MeuInt(int i){ inteiro = 1;}int MeuInt::operator+(int x){ inteiro = x + inteiro; return inteiro;}int MeuInt::operator-(int x){  inteiro = x - inteiro; return inteiro;}int MeuInt::operator*(int x){ inteiro = x * inteiro; return inteiro;}int MeuInt::operator/(int x){ inteiro = x / inteiro; return inteiro;}int MeuInt::getInteiro(){ return inteiro;}void MeuInt::setInteiro(int x){ inteiro = x;}calcDelta(MeuInt a, MeuInt b, MeuInt c){ delta = b*b - 4 * a * c return delta; }calcRaizes(MeuInt a, MeuInt b, MeuInt c, MeuInt delta){ x1 = (- b + sqrt(delta))/(2 * a) x2 = (- b - sqrt(delta))/(2 * a) return  }int main(int argc, char** argv) {  MeuInt inteiro1(10);  int c = inteiro1 + 1;  cout << "inteiro1 + 1: "; cout << c << endl;  cout << "Entre com as variáveis: " << endl; cout << "A: "; cin >> A;  cout << "B: "; cin >> B;  cout << "C: "; cin >> C;         return 0;}
+#include <iostream>
+using namespace std;
+
+class MeuInt{
+	
+	public:
+		MeuInt(int i=0);
+		int operator+(int);
+		int operator-(int);
+		int operator*(int);
+		int operator/(int);
+		int getInteiro();
+		void setInteiro(int);
+		
+	private:
+		int inteiro;
+		
+	
+};
+
+MeuInt::MeuInt(int i){
+	inteiro = 1;
+}
+
+
+
+int MeuInt::operator+(int x){
+	inteiro = x + inteiro;
+	return inteiro;
+}
+int MeuInt::operator-(int x){
+		inteiro = x - inteiro;
+	return inteiro;
+}
+int MeuInt::operator*(int x){
+	inteiro = x * inteiro;
+	return inteiro;
+}
+int MeuInt::operator/(int x){
+	inteiro = x / inteiro;
+	return inteiro;
+}
+
+int MeuInt::getInteiro(){
+	return inteiro;
+}
+
+void MeuInt::setInteiro(int x){
+	inteiro = x;
+}
+
+calcDelta(MeuInt a, MeuInt b, MeuInt c){
+	delta = b*b - 4 * a * c
+	return delta;
+	
+}
+
+
+
+int main(int argc, char** argv) {
+	
+	MeuInt inteiro1(10);
+	
+	int c = inteiro1 + 1;
+	
+	cout << "inteiro1 + 1: ";
+	cout << c << endl;
+	
+	cout << "Entre com as vari�veis: " << endl;
+	cout << "A: ";
+	cin >> A;
+	
+	cout << "B: ";
+	cin >> B;
+	
+	cout << "C: ";
+	cin >> C;
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
