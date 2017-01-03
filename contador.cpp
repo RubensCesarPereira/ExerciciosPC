865db8d1dd217a5ba5eb6c792061e07f9b3552df
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2017-01-03 18:23:38 -0200

    implementa pós fixado

--- contador.cpp
+++ contador.cpp
@@ -7,22 +7,34 @@ class Contador{
 	public:
 		Contador(int c = 0){contador = c;};
 		int getContador(){return contador;};
-		void operator++(){++contador;};
+		
+		Contador operator++(){ //pré-incremento
+			++contador;
+		//	Contador temp;
+		//	temp.contador = contador;
+			return contador; };
+			
+		Contador operator++(int){ //pós-incremento
+			return contador;
+			contador++;
+		}	
     private:
-    	int contador;
+    	unsigned int contador;
 };
 
 int main(){
 	
-	Contador c1, c2;
+	Contador c1, c2, c3;
 	
 	++c1;
 	++c2;
 	++c2;
 	++c2;
+	c3 = c2++;
 	
 	cout << "c1: " << c1.getContador() << endl;
 	cout << "c2: " << c2.getContador() << endl;	
+	cout << "c3: " << c3.getContador() << endl;	
 	
 	return 0;
 }
