bc03c73682db0371f49b633600c9d5ec5b9000e3
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2017-01-02 22:26:55 -0200

    Create ClassesDerivadasHeranca.cpp

---
+++ ClassesDerivadasHeranca.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+ class animal
+ {
+   public:
+      void comer();
+      void mover();
+      void dormir() { cout << "Dormindo..." << endl; }
+ };
+
+
+ class ave : public animal
+ {
+   public:
+      void comer(){ cout << "Bicando..." << endl; }
+      void mover(){ cout << "Voando..." << endl; }
+ };
+ class peixe : public animal
+ {
+   public:
+      void comer(){ cout << "Mordendo..." << endl; }
+      void mover(){ cout << "Nadando..." << endl; }
+ };
+
+int main()
+{
+  ave passarinho;
+  peixe sardinha;
+
+  passarinho.mover();
+  sardinha.mover();
+
+ return 0;
+}
